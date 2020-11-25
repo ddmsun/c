a323835e7d7a4b902a7c4079429181799d86156d
ddmsun <74295495+ddmsun@users.noreply.github.com>
2020-11-25 17:41:58 +0800

    Create 112505.小霸王猜数字.app.c

---
+++ 112505.小霸王猜数字.app.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{while(1){
+
+int a=5;
+int b;
+scanf ("%d",&b);
+if (b==a){
+	printf("恭喜你猜对了");}
+else if (b>a){
+	printf("你猜的太大了");}
+else 
+printf("你猜的太小了");
+
+
+}
+}
