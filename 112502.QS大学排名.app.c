439773e0d07ff679e5333b2b7d64372eba5b71da
ddmsun <74295495+ddmsun@users.noreply.github.com>
2020-11-25 12:01:00 +0800

    Create 112502.QS大学排名.app.c

---
+++ 112502.QS大学排名.app.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+int a;
+scanf("%d",&a);
+if (a==1){
+	printf ("清华大学");}
+else if (a==2){
+printf("北京大学");}
+else if (a==3){
+printf("复旦大学");}
+else if (a==4){
+printf("浙江大学");}
+else if (a==5){
+printf("南京大学");}
+else if (a==6){
+printf("上海交通大学");}
+else if (a==7){
+printf("华中科技大学");}
+else if (a==8){
+printf("中国科技大学");}
+else if (a==9){
+printf("中国人民大学");}
+else if (a==10){
+printf("武汉大学");}
+else  {
+printf("老铁输入的排名太靠后了，请输入1-10");}
+}
