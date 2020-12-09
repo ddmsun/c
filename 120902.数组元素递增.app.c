ffd0722feb6cf61c6f8f70ccf61a83cb944488ce
ddmsun <74295495+ddmsun@users.noreply.github.com>
2020-12-09 11:46:43 +0800

    Update 120902.数组元素递增.app.c

--- 120902.数组元素递增.app.c
+++ 120902.数组元素递增.app.c
@@ -1,15 +1,15 @@
+
 #include<stdio.h>
 void main(){ 
 int c=0;
 int	a,b;		
-	int array2d[9][10]={
-		{0,0,0,0,0,0,0,0,0,0},
+	int array2d[19][20]={
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 	};
-	for (a=0;a<9;a++){
-	for (b=0;b<10;b++){	
+	for (a=0;a<19;a++){
+	for (b=0;b<20;b++){	
 		c++;
 		array2d[a][b]=c;
 	printf ("%d ", array2d[a][b]);}
 	printf("\n");}
-			
-		}
+}	
